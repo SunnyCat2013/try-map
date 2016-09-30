e0940596a7695f1151c387b35966077e324db700
SunnyCat2013 <lizhenyang_2008@163.com>
2016-09-30 14:39:39 +0800

    counter: test

--- scatter-map/src/index.js
+++ scatter-map/src/index.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class Counter extends Component {
+  constructor () {
+    super();
+    this.state = {
+      counter: 0
+    };
+
+    this.addFun = this.addFun.bind(this);
+    this.minFun = this.minFun.bind(this);
+  }
+
+  addFun() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+  minFun() {
+    this.setState({
+      counter: this.state.counter - 1
+    })
+  }
   render () {
     return (
       <div>
-        hello webpack & react.
+        <button onClick={this.addFun}>+</button>
+        {this.state.counter}
+        <button onClick={this.minFun}>-</button>
       </div>
     );
   }
