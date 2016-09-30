08f0ff1a637ab61cb1711d9d2872b34327c89772
SunnyCat2013 <lizhenyang_2008@163.com>
2016-09-30 16:56:05 +0800

    global variable mode works

--- scatter-map/src/components/MGraph/MadaMap.js
+++ scatter-map/src/components/MGraph/MadaMap.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import L from '../../leaf-baidu/BaiduLayer.js';
+
+const DEFAULT_LATITUDE = 40.40;
+const DEFAULT_LONGITUDE = 116.45;
+
 class MadaMap extends React.Component {
   constructor () {
     super();
   }
+
+  componentDidMount () {
+    const map = new L.map(
+      'map',
+      {
+        'crs': L.CRS.BEPSG3857,
+      }
+    );
+
+    map.setView([DEFAULT_LATITUDE, DEFAULT_LONGITUDE], 13);
+    L.control.scale().addTo(map);
+    const baiduLayer = new L.TileLayer.BaiduLayer();
+    map.addLayer(baiduLayer);
+  }
+
+
   render () {
     return (
-      <div>
+      <div id="mada-map">
         I am MadaMap!
       </div>
     );
